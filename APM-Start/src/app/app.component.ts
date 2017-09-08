f5102b86f2ae3a36ce31fd2dd39e38569cd7067d
Sunil Srikanth <v-ssrikanth@expedia.com>
2017-09-08 17:41:03 +1000

    inital commit

---
+++ APM-Start/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pm-root',
+  template:`
+  <div><h1>{{pageTitle}}</h1> 
+  </div>`
+})
+export class AppComponent {
+  pageTitle: string = 'Acme Product Management'
+}
