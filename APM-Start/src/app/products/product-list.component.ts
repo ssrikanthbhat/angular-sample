ed80b07090c55766e21164cee53524cb0209ff8c
ssrikanthbhat <sunilsrikanth85@gmail.com>
2017-09-08 21:14:31 +1000

    adll images using property binding

--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -6,6 +6,8 @@ import { Component } from "@angular/core";
 })
 export class ProductListComponent {
   pageTitle: string = 'Produc List'
+  imageWidth: number = 50;
+  imageMargin: number =2;
   products: any[] = [{
     
         "productId": 1,
