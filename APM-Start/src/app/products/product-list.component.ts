c9453e6dd85f125e0d75e9d96f96d0df2f5723f1
ssrikanthbhat <sunilsrikanth85@gmail.com>
2017-09-08 21:25:07 +1000

    display or hide image based on event handling

--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -8,6 +8,7 @@ export class ProductListComponent {
   pageTitle: string = 'Produc List'
   imageWidth: number = 50;
   imageMargin: number =2;
+  showImage: boolean = false;
   products: any[] = [{
     
         "productId": 1,
@@ -30,4 +31,7 @@ export class ProductListComponent {
         "imageUrl": "http://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
     }];
   
+    toggleImage(): void{
+        this.showImage = !this.showImage
+    }
 }
