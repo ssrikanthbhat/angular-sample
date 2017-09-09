5039890b8494089d0644ad53f42ed5104c44e31d
ssrikanthbhat <sunilsrikanth85@gmail.com>
2017-09-09 19:29:02 +1000

    remove commented line

--- APM-Start/src/app/app.component.ts
+++ APM-Start/src/app/app.component.ts
@@ -20,18 +20,3 @@ import { ProductService } from "./products/product.service";
 export class AppComponent {
   pageTitle: string = 'Acme Product Management'
 }
-
-// <div>
-// <nav class='navbar navbar-default'>
-//     <div class='container-fluid'>
-//         <a class='navbar-brand'>{{pageTitle}}</a>
-//         <ul class='nav navbar-nav'>
-//             <li><a [routerLink]="['/welcome']">Home</a></li>
-//             <li><a [routerLink]="['/products']">Product List</a></li>
-//         </ul>
-//     </div>
-// </nav>
-// <div class='container'>
-//     <router-outlet></router-outlet>
-// </div>
-// </div>
