a9a0291872e69fc631e821c2b85f07553082932d
ssrikanthbhat <sunilsrikanth85@gmail.com>
2017-09-09 15:36:53 +1000

    Using a Using a Custom Pipe to remove spaces

--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -4,10 +4,11 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import {ProductListComponent} from './products/product-list.component'
+import {ConvertToSpacesPipe} from './products/ConvertToSpacesPipe'
 
 @NgModule({
   declarations: [
-    AppComponent,ProductListComponent
+    AppComponent,ProductListComponent, ConvertToSpacesPipe
   ],
   imports: [
     BrowserModule,
