286a07db181e8f5816bfb190674c008fb3b55c97
ssrikanthbhat <sunilsrikanth85@gmail.com>
2017-09-09 18:39:04 +1000

    Subscribe to observable and call HTTP request by registering providers

--- APM-Start/src/app/products/product.service.ts
+++ APM-Start/src/app/products/product.service.ts
@@ -1,9 +1,13 @@
 import { IProduct } from "./product";
 import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs/Observable";
 
 @Injectable()
 export class ProductService {
-    getProducts(): IProduct[] {
-        return null
+    private productServiceUrl = './api/products/products.json'
+    constructor(private httpClient : HttpClient){}
+    getProducts(): Observable<IProduct[]> {
+        return this.httpClient.get<IProduct[]>(this.productServiceUrl);
     }
 }
