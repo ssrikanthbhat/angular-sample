1d878c5e1f75073075664840f5c58778f458f610
ssrikanthbhat <sunilsrikanth85@gmail.com>
2017-09-09 15:08:20 +1000

    Create interface fro strong typing

--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -1,5 +1,5 @@
 import { Component } from "@angular/core";
-
+import { IProduct } from "./product";
 @Component({
     selector: 'pm-products',
     templateUrl: './product-list.component.html'
@@ -10,17 +10,7 @@ export class ProductListComponent {
   imageMargin: number =2;
   showImage: boolean = false;
   listFilter: string = 'cart';
-  products: any[] = [{
-    
-        "productId": 1,
-        "productName": "Leaf Rake",
-        "productCode": "GDN-0011",
-        "releaseDate": "March 19, 2016",
-        "description": "Leaf rake with 48-inch wooden handle.",
-        "price": 19.95,
-        "starRating": 3.2,
-        "imageUrl": "http://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png"
-    },
+  products: IProduct[] = [
     {
         "productId": 2,
         "productName": "Garden Cart",
@@ -30,7 +20,18 @@ export class ProductListComponent {
         "price": 32.99,
         "starRating": 4.2,
         "imageUrl": "http://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
-    }];
+    },
+    {
+        "productId": 5,
+        "productName": "Hammer",
+        "productCode": "TBX-0048",
+        "releaseDate": "May 21, 2016",
+        "description": "Curved claw steel hammer",
+        "price": 8.9,
+        "starRating": 4.8,
+        "imageUrl": "http://openclipart.org/image/300px/svg_to_png/73/rejon_Hammer.png"
+    }
+];
   
     toggleImage(): void{
         this.showImage = !this.showImage
