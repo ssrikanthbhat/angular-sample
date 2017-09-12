91de237603483fdc3bf3d990878e5cbdb83064b7
ssrikanthbhat <sunilsrikanth85@gmail.com>
2017-09-12 18:59:17 +1000

    Use the Router service  Create a method that calls the navigate method of the Router service Add a user interface element Build a guard service Register the guard service provider

--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -1,35 +1,41 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import {ProductListComponent} from './products/product-list.component'
-import {ConvertToSpacesPipe} from './products/ConvertToSpacesPipe'
-import { StarComponent } from "./shared/star.component";
-import { HttpClientModule } from "@angular/common/http";
-import {RouterModule} from "@angular/router"
-import { ProductDetailComponent } from "./products/product-detail.component";
-import { WelcomeComponent } from "./home/welcome.component";
-import { PageNotFoundComponent } from "./home/pagenotfound.component";
+import { ProductListComponent } from './products/product-list.component';
+import { StarComponent } from './shared/star.component';
+import { ProductDetailComponent } from './products/product-detail.component';
+import { WelcomeComponent } from './home/welcome.component';
+import { ProductGuardService } from './products/product-guard.service';
+import { ConvertToSpacesPipe } from "./products/ConvertToSpacesPipe";
 
 @NgModule({
   declarations: [
-    AppComponent,ProductListComponent, ConvertToSpacesPipe, StarComponent, WelcomeComponent, ProductDetailComponent
+    AppComponent,
+    ProductListComponent,
+    ConvertToSpacesPipe,
+    StarComponent,
+    ProductDetailComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpClientModule,
-    RouterModule.forRoot(
-      [
+    RouterModule.forRoot([
         { path: 'products', component: ProductListComponent },
-        { path: 'products/:id', component: ProductDetailComponent },
+        { path: 'products/:id',
+          canActivate: [ ProductGuardService ],
+          component: ProductDetailComponent },
         { path: 'welcome', component: WelcomeComponent },
-        { path: '', redirectTo: 'welcome', pathMatch: 'full' },
-        { path: '**', component: PageNotFoundComponent }
-      ],{ useHash: true })
+        { path: '', redirectTo: 'welcome', pathMatch: 'full'},
+        { path: '**', redirectTo: 'welcome', pathMatch: 'full'}
+    ])
   ],
-  providers: [],
+  providers: [ProductGuardService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
